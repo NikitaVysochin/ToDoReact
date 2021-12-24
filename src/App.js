af13b6f7145c086040e61451d77415d51da1e560
Nikita <nikitavysochin.exceedteam@gmail.com>
2021-12-24 13:58:36 +0300

    final version

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from "./logo.svg";
 import "./App.scss";
-import React, { State, useState } from "react";
+import React, { State, useEffect, useState } from "react";
+import axios from "axios";
 import Test from "./Header/Header";
-import MainInput from "./MainInput/MainInput"
+import MainInput from "./MainInput/MainInput";
 import Tasks from "./Tasks/Tasks";
 
 const App = () => {
-  let temp = [
+  const [arr, setArr] = useState([]);
+
+  /*let temp = [
     {
       taskName: "задача",
       isCheck: false,
@@ -15,20 +18,22 @@ const App = () => {
       taskName: "задача3",
       isCheck: false,
     }
-  ];
-  const [arr, setArr] = useState(temp);
+  ];*/
 
-  return (<div>
-    <Test/>
-    <MainInput 
-      arr={arr} 
-      setArr={setArr}
-    />
-    <Tasks 
-      arr={arr} 
-      setArr={setArr} 
-    />
-  </div>);
-}
+  useEffect(async () => {
+    await axios.get("http://localhost:8000/getAllTasks").then((res) => {
+      setArr(res.data.data);
+    });
+  });
+
+  
+  return (
+    <div>
+      <Test />
+      <MainInput arr={arr} setArr={setArr} />
+      <Tasks arr={arr} setArr={setArr} />
+    </div>
+  );
+};
 
 export default App;
